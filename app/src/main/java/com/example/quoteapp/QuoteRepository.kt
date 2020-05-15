b3ea0cefd133b65c0e8b96fa346e8fb01e52a2c7
mirzacharlie <53055387+mirzacharlie@users.noreply.github.com>
2020-05-15 16:57:03 +0300

    Загружается по 10 цитат за раз

--- app/src/main/java/com/example/quoteapp/QuoteRepository.kt
+++ app/src/main/java/com/example/quoteapp/QuoteRepository.kt
@@ -15,32 +15,58 @@ class QuoteRepository(private val apiService: ApiService, private val quoteDao: 
         get() = Dispatchers.Main
 
 
-    //выкачиваю цитату и отправляю на валидацию
-    fun loadNewQuote(){
+    //  Загружает и добавляет в БД 10 цитат
+    fun loadNewQuotes() {
         launch {
-            val quote: Quote = apiService.loadNewQuoteFromInternet()
-            validate(quote)
+            val quoteList: MutableList<Quote> = mutableListOf()
+            val startKey = getLastQuoteId()+1
+
+            while (quoteList.size < 10){
+                val quote = withContext(Dispatchers.Default) {
+                    apiService.loadNewQuoteFromInternet(key = (startKey+quoteList.size).toString())
+                }
+                if(validate(quote)){
+                    quoteList.add(quote)
+                }
+            }
+            insertQuoteList(quoteList)
         }
     }
 
-    fun getAllQuotes() = quoteDao.getAllQuotes()
+    //  выкачиваю цитату и отправляю на валидацию
+    private fun loadNewQuote(key: String) {
+        launch {
+            val quoteTMP = async { apiService.loadNewQuoteFromInternet(key = key) }
+            val quote = quoteTMP.await()
+        }
+    }
 
-    private suspend fun insertQuotes(quote: Quote) {
-        withContext(Dispatchers.IO){
+    //  Возвращает Id последней записи или 0 при пустоой таблице
+    private suspend fun getLastQuoteId(): Long {
+        val quote = coroutineScope { async { quoteDao.qetLastQuote() } }.await()
+        return if(quote?.id != null){
+            quote.id
+        } else {
+            0L
+        }
+    }
+
+    private suspend fun insertQuote(quote: Quote) {
+        withContext(Dispatchers.IO) {
             quoteDao.addQuote(quote)
         }
     }
 
-    //Если текст или автор цитаты пустые, то выкачиваю новую цитату. Если всё ок, то добавляю цитату в БД
-    private fun validate(quote: Quote){
-        if(quote.quoteText !=null && quote.quoteText.isNotEmpty() &&
-            quote.quoteAuthor != null && quote.quoteAuthor.isNotEmpty()){
-            launch {
-                insertQuotes(quote)
-            }
-        } else {
-            loadNewQuote()
+    private suspend fun insertQuoteList(quotes: List<Quote>) {
+        withContext(Dispatchers.IO) {
+            quoteDao.addQuoteList(quotes)
         }
+    }
+
+    //  Возвращает True если текст и автор не пустые
+    private fun validate(quote: Quote): Boolean {
+        return quote.quoteText != null && quote.quoteText.isNotEmpty() &&
+                quote.quoteAuthor != null && quote.quoteAuthor.isNotEmpty()
     }
 
 }

36ad4a0c8acf61fe946617d254de0726ab6d0755
Mirzacharlie <mirzacharlie@gmail.com>
2020-07-07 16:24:45 +0300

    Добавил виджет

--- app/src/main/java/com/example/quoteapp/QuoteWidget.kt
+++ app/src/main/java/com/example/quoteapp/QuoteWidget.kt
@@ -4,10 +4,14 @@ import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.widget.RemoteViews
 import com.example.quoteapp.QuoteWidget.Companion.ACTION_REFRESH_CLICKED
 import com.example.quoteapp.QuoteWidget.Companion.NAME_FAV
 import com.example.quoteapp.QuoteWidget.Companion.NAME_ID
+import com.example.quoteapp.pojo.Quote
+import com.example.quoteapp.utils.APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID
+import com.example.quoteapp.utils.APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE
 
 
 class QuoteWidget : QuoteWidgetProvider() {
@@ -18,17 +22,19 @@ class QuoteWidget : QuoteWidgetProvider() {
 
         const val NAME_ID = "id"
         const val NAME_FAV = "fav"
+        const val NAME_LOAD_NEW_QUOTE = "load new quote"
     }
 
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager,
+                          appWidgetIds: IntArray
     ) {
-
         for (appWidgetId in appWidgetIds) {
             updateAppWidget(
                 context,
                 appWidgetManager,
                 appWidgetId,
-                repository
+                repository,
+                preferences
             )
         }
     }
@@ -39,10 +45,24 @@ internal fun updateAppWidget(
     context: Context,
     appWidgetManager: AppWidgetManager,
     appWidgetId: Int,
-    repository: QuoteRepository
+    repository: QuoteRepository,
+    preferences: SharedPreferences
 ) {
     val views = RemoteViews(context.packageName, R.layout.quote_widget)
-    val quote = repository.getRandomQuote()
+    lateinit var quote: Quote
+
+    if (preferences.contains(APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE)){
+        if (preferences.getBoolean(APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE, false)){
+            quote = repository.getRandomQuote()
+        } else if (preferences.contains(APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID)) {
+            quote = repository.getQuoteById(preferences.getLong(
+                APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID, 1))
+        }
+    } else {
+        quote = repository.getRandomQuote()
+    }
+
+    quote.quoteId?.let { preferences.edit().putLong(APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID, it) }
 
     views.setTextViewText(R.id.tv_quote, quote.quoteText)
     views.setTextViewText(R.id.tv_author, quote.quoteAuthor)
@@ -61,10 +81,11 @@ internal fun updateAppWidget(
         context, appWidgetId, intentUpdate,
         PendingIntent.FLAG_UPDATE_CURRENT
     )
-    val newFav = if (quote.isFavourite == 0) 1 else 0
 
     val intentFav = Intent(context, QuoteWidget::class.java)
     intentFav.action = QuoteWidget.ACTION_FAV_CLICKED
+    intentFav.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, idArray)
+    val newFav = if (quote.isFavourite == 0) 1 else 0
     intentFav.putExtra(NAME_ID, quote.quoteId)
     intentFav.putExtra(NAME_FAV, newFav)
     val pendingFav  = PendingIntent.getBroadcast(
@@ -73,5 +94,6 @@ internal fun updateAppWidget(
 
     views.setOnClickPendingIntent(R.id.iv_refesh_button, pendingUpdate)
     views.setOnClickPendingIntent(R.id.iv_fav_button, pendingFav)
+
     appWidgetManager.updateAppWidget(appWidgetId, views)
 }
--- app/src/main/java/com/example/quoteapp/QuoteWidgetProvider.kt
+++ app/src/main/java/com/example/quoteapp/QuoteWidgetProvider.kt
@@ -4,6 +4,10 @@ import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import com.example.quoteapp.QuoteWidget.Companion.ACTION_REFRESH_CLICKED
+import com.example.quoteapp.utils.APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID
+import com.example.quoteapp.utils.APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE
 import dagger.android.AndroidInjection
 import javax.inject.Inject
 
@@ -11,6 +15,7 @@ import javax.inject.Inject
 open class QuoteWidgetProvider : AppWidgetProvider() {
 
     @Inject lateinit var repository: QuoteRepository
+    @Inject lateinit var preferences: SharedPreferences
 
     override fun onReceive(context: Context, intent: Intent) {
         AndroidInjection.inject(this, context);
@@ -20,18 +25,23 @@ open class QuoteWidgetProvider : AppWidgetProvider() {
         if (QuoteWidget.ACTION_FAV_CLICKED == action){
             val id = intent.getLongExtra(QuoteWidget.NAME_ID, 0)
             val fav = intent.getIntExtra(QuoteWidget.NAME_FAV, 0)
-            repository.updateFavouriteByID(id, fav)
-        } else if (QuoteWidget.ACTION_REFRESH_CLICKED == action) {
-            val extras = intent.extras
-            if (extras != null) {
-                val appWidgetIds = extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS)
-                if (appWidgetIds != null && appWidgetIds.isNotEmpty()) {
-                    onUpdate(context, AppWidgetManager.getInstance(context), appWidgetIds)
-                }
-            }
+            repository.updateFavouriteByIdWithBlocking(id, fav)
+            preferences.edit().putBoolean(APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE, false).apply()
+            preferences.edit().putLong(APP_PREFERENCES_WIDGET_CURRENT_QUOTE_ID, id).apply()
+        } else if(ACTION_REFRESH_CLICKED == action){
+            preferences.edit().putBoolean(APP_PREFERENCES_WIDGET_LOAD_NEW_QUOTE, true).apply()
         }
 
+        val extras = intent.extras
+        if (extras != null) {
+            val appWidgetIds = extras.getIntArray(AppWidgetManager.EXTRA_APPWIDGET_IDS)
+            if (appWidgetIds != null && appWidgetIds.isNotEmpty()) {
+                onUpdate(context, AppWidgetManager.getInstance(context), appWidgetIds)
+            }
+        }
         super.onReceive(context, intent)
     }
+
+
 
 }
